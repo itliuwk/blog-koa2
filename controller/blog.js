01d789fc539ba7343b789a9f33809e9ed0f27301
liuwk <1834183435@qq.com>
2019-09-24 18:05:19 +0800

    增加 文章统计数量 sql

--- controller/blog.js
+++ controller/blog.js
@@ -1,4 +1,6 @@
-const {exec} = require('../db/mysql');
+const {
+	exec
+} = require('../db/mysql');
 
 
 /**
@@ -12,23 +14,23 @@ const {exec} = require('../db/mysql');
  */
 const getList = async (author, keyword, classify, page, total) => {
 
-    let sql = ` select * from blogs a ,classify b where 1=1 `;
+	let sql = ` select * from blogs a ,classify b where 1=1 `;
 
-    if (author) {
-        sql += `and a.author='${author}' `
-    }
-    if (keyword) {
-        sql += `and a.title like '%${keyword}%' `
-    }
+	if (author) {
+		sql += `and a.author='${author}' `
+	}
+	if (keyword) {
+		sql += `and a.title like '%${keyword}%' `
+	}
 
-    sql += `and a.classify =b.value order by a.createtime desc `;
+	sql += `and a.classify =b.value order by a.createtime desc `;
 
-    if (page) {
-        sql += ` LIMIT ${page},${total};`
-    }
+	if (page) {
+		sql += ` LIMIT ${page},${total};`
+	}
 	console.log(sql)
 
-    return await exec(sql);
+	return await exec(sql);
 };
 
 
@@ -41,18 +43,18 @@ const getList = async (author, keyword, classify, page, total) => {
 const getListCount = async (author, keyword) => {
 
 
-    let sql = `select count(id) from blogs where 1=1 `;
-    if (author) {
-        sql += `and author='${author}' `
-    }
-    if (keyword) {
-        sql += `and title like '%${keyword}%' `
-    }
+	let sql = `select count(id) from blogs where 1=1 `;
+	if (author) {
+		sql += `and author='${author}' `
+	}
+	if (keyword) {
+		sql += `and title like '%${keyword}%' `
+	}
 
 
-    return await exec(sql).then(row => {
-        return row[0];
-    })
+	return await exec(sql).then(row => {
+		return row[0];
+	})
 };
 
 
@@ -65,16 +67,16 @@ const getListCount = async (author, keyword) => {
  */
 const getClassify = async (classify, page, total) => {
 
-    let sql = ` select * from blogs a,classify b where 1=1 `;
+	let sql = ` select * from blogs a,classify b where 1=1 `;
 
 
-    sql += `and a.classify=${classify} and a.classify =b.value  order by createtime desc `;
+	sql += `and a.classify=${classify} and a.classify =b.value  order by createtime desc `;
 
-    if (page) {
-        sql += ` LIMIT ${page},${total};`
-    }
+	if (page) {
+		sql += ` LIMIT ${page},${total};`
+	}
 
-    return await exec(sql);
+	return await exec(sql);
 };
 
 /**
@@ -86,18 +88,18 @@ const getClassify = async (classify, page, total) => {
  */
 const getClassifyCount = async (classify, page, total) => {
 
-    let sql = ` select count(id) from blogs a , classify b where 1=1 `;
+	let sql = ` select count(id) from blogs a , classify b where 1=1 `;
 
 
-    sql += `and a.classify=${classify} and a.classify =b.value  order by createtime desc `;
+	sql += `and a.classify=${classify} and a.classify =b.value  order by createtime desc `;
 
-    if (page) {
-        sql += ` LIMIT ${page},${total};`
-    }
+	if (page) {
+		sql += ` LIMIT ${page},${total};`
+	}
 
-    return await exec(sql).then(row => {
-        return row[0];
-    })
+	return await exec(sql).then(row => {
+		return row[0];
+	})
 };
 
 
@@ -108,14 +110,18 @@ const getClassifyCount = async (classify, page, total) => {
  */
 const getDetail = async (id) => {
 
-    // let sql = ` select * from blogs where id = '${id}' `;
-    let sql = ` select * from blogs a , classify b where a.id=${id} and b.value =a.classify;`;
+	// let sql = ` select * from blogs where id = '${id}' `;
+	let sql = ` select * from blogs a , classify b where a.id=${id} and b.value =a.classify;`;
 
-    // 返回 promise
+	let countSql = ` UPDATE blogs SET count=(count+1) WHERE id= '${id}' `
 
-    return await exec(sql).then(row => {
-        return row[0];
-    })
+
+	exec(countSql)
+	// 返回 promise
+
+	return await exec(sql).then(row => {
+		return row[0];
+	})
 };
 
 
@@ -125,54 +131,68 @@ const getDetail = async (id) => {
  * @returns {{id: number}}
  */
 const newBlog = async (blogData) => {
-    // blogData  是一个博客  对象  包含title content 属性
-    const {title, subtitle, content, classify, author, createTime = Date.now()} = blogData;
+	// blogData  是一个博客  对象  包含title content 属性
+	const {
+		title,
+		subtitle,
+		content,
+		classify,
+		author,
+		createTime = Date.now()
+	} = blogData;
 
-    // let sql = ` insert into blogs (title,content,subtitle,createtime,author) values('${title}',"${content}",'${subtitle}',${createTime},'${author}');`;
+	// let sql = ` insert into blogs (title,content,subtitle,createtime,author) values('${title}',"${content}",'${subtitle}',${createTime},'${author}');`;
 
-    let sql = ` insert into blogs (title,content,subtitle,createtime,author,classify) values('${title}',"${content}",'${subtitle}',${createTime},'${author}','${classify}');`;
+	let sql =
+		` insert into blogs (title,content,subtitle,createtime,author,classify) values('${title}',"${content}",'${subtitle}',${createTime},'${author}','${classify}');`;
 
 
-    return await exec(sql).then(insertData => {
-        return {
-            id: insertData.insertId
-        }
-    });
+	return await exec(sql).then(insertData => {
+		return {
+			id: insertData.insertId
+		}
+	});
 };
 
 
 const updateBlog = async (id, blogData) => {
-    const {title, subtitle, content,classify} = blogData;
+	const {
+		title,
+		subtitle,
+		content,
+		classify
+	} = blogData;
 
-    let sql = ` update blogs set title='${title}', subtitle='${subtitle}', content='${content}' , classify='${classify}' where id=${id}`;
+	let sql =
+		` update blogs set title='${title}', subtitle='${subtitle}', content='${content}' , classify='${classify}' where id=${id}`;
 
-    const updateData = await exec(sql);
+	const updateData = await exec(sql);
 
-    if (updateData.affectedRows > 0) {
-        return true;
-    } else {
-        return true;
-    }
+	if (updateData.affectedRows > 0) {
+		return true;
+	} else {
+		return true;
+	}
 };
 const delBlog = async (id, author) => {
-    let sql = ` delete from blogs where id='${id}' and author='${author}'`;
+	let sql = ` delete from blogs where id='${id}' and author='${author}'`;
 
-    return await exec(sql).then(delData => {
-        if (delData.affectedRows > 0) {
-            return true;
-        } else {
-            return true;
-        }
-    });
+	return await exec(sql).then(delData => {
+		if (delData.affectedRows > 0) {
+			return true;
+		} else {
+			return true;
+		}
+	});
 };
 
 module.exports = {
-    getList,
-    getListCount,
-    getClassify,
-    getClassifyCount,
-    getDetail,
-    newBlog,
-    updateBlog,
-    delBlog
+	getList,
+	getListCount,
+	getClassify,
+	getClassifyCount,
+	getDetail,
+	newBlog,
+	updateBlog,
+	delBlog
 };
